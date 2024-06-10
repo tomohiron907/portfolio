95178f29c9bfbac2b3dcaedb3e6255cf99c40ae0
tomohiron907 <tomohiron907@gmail.com>
2024-06-11 00:47:37 +0900

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', 
   plugins: [react()],
   build: {
     outDir: 'docs',  // 出力ディレクトリを`docs`に設定
